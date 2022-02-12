d6f95a367bba68fbe5a0a8ae40b2aa5d2fb2179e
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-02-12 03:41:18 -0500

    still working on building my logic, line 41 is my break

--- assets/js/Logic.js
+++ assets/js/Logic.js
@@ -15,31 +15,41 @@ const codeSnow = [600,601,602,611,612,613,615,616,620,621,622];//MAIN: Snow
 console.log(codeSnow);
 const codeAtmosphere = [701,711,721,741,771]; //MAIN: Mist, Smoke, Haze, Fog, Squall
 console.log(codeAtmosphere);
+var badDay = badWeatherConditions;
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~Good Weather~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~OpenWeatherAPI
 const codeClear = [800]; //MAIN: Clear
 console.log(codeClear);
 const codeClouds = [801,802,803,804];// MAIN: Clouds (11-25%),Clouds (25-50%), Clouds (51-84%), Clouds (85-100%),
 console.log(codeClouds);
+var goodDay = goodWeatherConditions;
+var 
 //! JAVASCRIPT METHODS: toString(converts array to string), join(joins all array elems.), pop(pop out last array value), push(add new element to end of array),
 //! shift(removes first element and relists new array shifted index), unshift(adds new element to start of array), ARRAY[INDEX POSITION]
 //! concat(creates a new array my merging existing arrays), splice(adds new item to array), slice(slice out peice of an array)//The first parameter (2) defines the position where new elements should be added (spliced in)
 //! toString(converts array to comma seperated string), 
 //-------------------------------Activity Types-----------------------------------------------------------------RapidAPI[Trueway-Places]
 const goodWeatherActivities = [amusement_park,aquarium,bicycle_store,campground,park,stadium,tourist_attraction,zoo];
+var outsideThings = goodWeatherActivities;
 const badWeatherActivities = [art_gallery,bakery,bar,beauty_salon,book_store,bowling,cafe,casino,cinema,clothing_store,department_store,library,liquor_store,museum,night_club,restaurant,shoe_store,shopping_center,spa,store,gym,home_goods_store];
+var insideThings =badWeatherActivities;
 //! COMPARISONS, BOOLEANS, CONDITIONS, STATEMENTS[SWITCH], LOOPS[FOR, FOR IN, FOR OF++[ITERABLES]++ ,WHILE], SETS.add(), Map()
 //--A---Logic for comparing array output
-function compareWeatherConditions(){//!sTART OF FN////|||||
+function compareWeatherConditions(){//!START OF FN////|||||
 // IF-ELSEIF-ELSE compare weather conditions using booleans    
-if (goodWeatherConditions == true) {
-    alert('Tis a nice day my dude!');   
+
+if (goodDay == true) {
+    //! left off to bed  /let good= {
+    }
+    alert('goodWeatherActivities');   
 }
-else if(badWeatherConditions == true) {
-    alert('Tis a nice day my dude!');
+else if(badDay == true) {
+    insideThings;
+    alert('badWeatherActivities');
 }
 else {
     alert('Houston we have a problem!');
 };
+return;
 };//!END OF FN////////////////////////////////////////||||
 //--B-1--Logic based on types based on weather
 
